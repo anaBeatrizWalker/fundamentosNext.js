6c6b47f580229d0d4511f063246a3619dfd361b2
Ana Beatriz <anabiawalker@gmail.com>
2021-12-22 15:17:45 -0300

    Adc 3º componente (css global)

--- fundamentos/src/pages/_app.jsx
+++ fundamentos/src/pages/_app.jsx
@@ -1,4 +1,6 @@
+//Arquivos globais são importados nesse arquivo
 import '../styles/globals.css'
+import '../styles/app.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
